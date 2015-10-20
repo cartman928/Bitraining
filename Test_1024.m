d0fb3aee37fa8ed2c3836d8dc7c62229a2c8cf0f
Shao-Han Chen <Shao-Han.Chen@u.northwestern.edu>
2015-10-19 21:39:03 -0500

    ADD
    ADD

--- Test_1024.m
+++ Test_1024.m
@@ -1,13 +1,16 @@
+%2 user, 2X2 MIMO Channel
+%SINR
 clc
 clear
 
-gc(:,1,1) = [-0.3520 - 0.1639i;0.2730 + 0.6405i];
-gp(:,1,1) = [0.1697 + 0.2841i;0.1733 + 0.1567i];
-gc(:,2,1) = [-0.1478 - 0.0519i;-0.0879 - 0.4007i];
-gp(:,2,1) = [0.2490 + 0.3453i;-0.0598 - 0.1202i];
+gc(:,1) = [-0.3520 - 0.1639i;0.2730 + 0.6405i];
+gp(:,1) = [0.1697 + 0.2841i;0.1733 + 0.1567i];
+gc(:,2) = [-0.1478 - 0.0519i;-0.0879 - 0.4007i];
+gp(:,2) = [0.2490 + 0.3453i;-0.0598 - 0.1202i];
 
 gc(:,1)=gc(:,1)/norm(gc(:,1));
 gc(:,2)=gc(:,2)/norm(gc(:,2));
+
 gp(:,1)=gp(:,1)/norm(gp(:,1));
 gp(:,2)=gp(:,2)/norm(gp(:,2));
 
@@ -18,8 +21,10 @@ vp(:,2) = [-0.1296 - 0.1773i;0.3754 + 0.5062i];
 
 vc(:,1)=vc(:,1)/norm(vc(:,1));
 vc(:,2)=vc(:,2)/norm(vc(:,2));
+
 vp(:,1)=vp(:,1)/norm(vp(:,1));
 vp(:,2)=vp(:,2)/norm(vp(:,2));
+
 
 H{1,1}=(1/sqrt(2))*[-0.9704 + 0.4012i (1.9144 - 0.3561i);(0.4516 - 1.4800i) 0.0501 - 0.1627i];
 H{1,2}=(1/sqrt(2))*[-0.6337 + 0.8001i (-1.1485 + 0.2689i);(0.4516 - 1.4800i) -0.6651 - 0.9268i];
@@ -55,8 +60,11 @@ for k = 1:2
                   
             gc_wiener(:,k) = inv(  H{k,k}*vc(:,k)*vc(:,k)'*H{k,k}' + H{k,k}*vp(:,k)*vp(:,k)'*H{k,k}' + sum_c1_f(:,k)*sum_c1_f(:,k)' +sum_p1_f(:,k)*sum_p1_f(:,k)' + H{k,k}*vc(:,k)*sum_c1_f(:,k)'+sum_c1_f(:,k)*vc(:,k)'*H{k,k}'+eye(2)*sigma^2  ) * ( sum_c2_f(:,k) );
             gc_wiener(:,k) = gc_wiener(:,k)/norm(gc_wiener(:,k));
+            gc_wiener(:,k) = [0;0];
             gp_wiener(:,k) = inv(  H{k,k}*vc(:,k)*vc(:,k)'*H{k,k}' + H{k,k}*vp(:,k)*vp(:,k)'*H{k,k}' + sum_c1_f(:,k)*sum_c1_f(:,k)' +sum_p1_f(:,k)*sum_p1_f(:,k)' + H{k,k}*vc(:,k)*sum_c1_f(:,k)'+sum_c1_f(:,k)*vc(:,k)'*H{k,k}'+eye(2)*sigma^2  ) * ( H{k,k}*vp(:,k) );
             gp_wiener(:,k) = gp_wiener(:,k)/norm(gp_wiener(:,k));
+           
+
            
 end
 
@@ -84,8 +92,10 @@ for k = 1:2
                   
             vc_wiener(:,k) = inv(  H{k,k}.'*gc_wiener(:,k)*gc_wiener(:,k)'*(H{k,k}').' + H{k,k}.'*gp_wiener(:,k)*gp_wiener(:,k)'*(H{k,k}').' + sum_c1_b(:,k)*sum_c1_b(:,k)' +sum_p1_b(:,k)*sum_p1_b(:,k)' + H{k,k}.'*gc_wiener(:,k)*sum_c1_b(:,k)'+sum_c1_b(:,k)*gc_wiener(:,k)'*(H{k,k}').'+eye(2)*sigma^2  ) * ( sum_c2_b(:,k) ); 
             vc_wiener(:,k) = vc_wiener(:,k)/norm(vc_wiener(:,k));
+            vc_wiener(:,k) = [0;0];
             vp_wiener(:,k) = inv(  H{k,k}.'*gc_wiener(:,k)*gc_wiener(:,k)'*(H{k,k}').' + H{k,k}.'*gp_wiener(:,k)*gp_wiener(:,k)'*(H{k,k}').' + sum_c1_b(:,k)*sum_c1_b(:,k)' +sum_p1_b(:,k)*sum_p1_b(:,k)' + H{k,k}.'*gc_wiener(:,k)*sum_c1_b(:,k)'+sum_c1_b(:,k)*gc_wiener(:,k)'*(H{k,k}').'+eye(2)*sigma^2  ) * ( H{k,k}.'*gp_wiener(:,k) );
             vp_wiener(:,k) = vp_wiener(:,k)/norm(vp_wiener(:,k));
+           
             
             
 end
@@ -99,6 +109,6 @@ SINR_P(2)=(   norm( gp_wiener(:,2)'*H{2,2}*vp_wiener(:,2) )^2   )/( norm( gp_wie
 C2=0;
 for i = 1:2
     
-    C2=C2+log2(1+SINR_C(i))+log2(1+SINR_P(i))
+    C2=C2+log2(1+SINR_P(i))
     
 end
