d01cbe0ca3e336b9b38dafd485db4b231654ff97
Shao-Han Chen <Shao-Han.Chen@u.northwestern.edu>
2015-10-20 19:42:29 -0500

    add

--- Plot.m
+++ Plot.m
@@ -1,5 +1,5 @@
 %function [C] = Test_1026(times,sigma,StepSize,k,z)
-z=10^(2);Realization=2;StepSize=2*10^(-3);
+z=2*10^(2);Realization=2;StepSize=2*10^(-3);
 
 [C1] = Test_1026(Realization,sqrt(10^(-3)),StepSize,1,z)
 [C2] = Test_1026(Realization,sqrt(10^(-3)),StepSize,2,z)
@@ -10,6 +10,12 @@ z=10^(2);Realization=2;StepSize=2*10^(-3);
 [C7] = Test_1025(Realization,sqrt(10^(-3)),StepSize,4,z)
 [C8] = Test_1025(Realization,sqrt(10^(-3)),StepSize,8,z)
 
+
+
 x=1:z;
-plot(x,C1(x),x,C2(x),x,C3(x),x,C4(x),x,C5(x),x,C6(x),x,C7(x),x,C8(x))
-legend('C1(co-op)','C2(co-op)','C3(co-op)','C4(co-op)','C5','C6','C7','C8')
+
+C9(x)= 3.35;
+C10(x)= 3.15;
+
+plot(x,C10(x),x,C9(x),x,C1(x),x,C2(x),x,C3(x),x,C4(x),x,C5(x),x,C6(x),x,C7(x),x,C8(x))
+legend('co-op','not co-op','C1(co-op)','C2(co-op)','C3(co-op)','C4(co-op)','C5','C6','C7','C8')
