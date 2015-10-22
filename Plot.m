570f9055d0788f8059029d35d808481a03301091
Shao-Han Chen <Shao-Han.Chen@u.northwestern.edu>
2015-10-22 12:36:31 -0500

    aadd

--- Plot.m
+++ Plot.m
@@ -1,5 +1,5 @@
 %function [C] = Test_1026(times,sigma,StepSize,k,z)
-z=2*10^(2);Realization=2;StepSize=2*10^(-3);
+z=3*10^(2);Realization=2;StepSize=2*10^(-3);
 
 [C1] = Test_1026(Realization,sqrt(10^(-3)),StepSize,1,z)
 [C2] = Test_1026(Realization,sqrt(10^(-3)),StepSize,2,z)
@@ -19,3 +19,5 @@ C10(x)= 3.15;
 
 plot(x,C10(x),x,C9(x),x,C1(x),x,C2(x),x,C3(x),x,C4(x),x,C5(x),x,C6(x),x,C7(x),x,C8(x))
 legend('Wiener co-op','Wiener','C1(co-op)','C2(co-op)','C3(co-op)','C4(co-op)','C5','C6','C7','C8')
+xlabel('Training Length') % x-axis label
+ylabel('C(bit/channel)') % y-axis label
