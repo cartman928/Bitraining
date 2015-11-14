1ad11bc49d55d58a9fd0a4f9c32c0fe7642786e3
Shao-Han Chen <Shao-Han.Chen@u.northwestern.edu>
2015-11-13 21:30:37 -0600

    add

--- LS_backward.m
+++ LS_backward.m
@@ -1,22 +1,32 @@
-function [Vu, Vm] = LS_backward(H, Gu, Gm, M2, n0, Bu, Bm, upower, mpower)
+function [Vu, Vm] = LS_backward(H, Gu, Gm, M2, n0, Bu, Bm, upower, mpower,nnnn)
 %update receive filters by least square algorithm
-%[Vu, Vm] = LS_backward(H, Gu, Gm, M2, n0, Bbw, BbwBr, upower, mpower);
-
 
 [Nr,Nt,M,ignore] = size(H);
 
-%M=2, M2=10
+Y = zeros(Nt,M2,M);
 
+Vu = zeros(Nt,M);
+Vm = zeros(Nt,M);
 for user_idx = 1 : M
     for sym_idx = 1:M2
-        Y(:,sym_idx,user_idx) = H(:,:,user_idx,user_idx)'*Gu(:,user_idx)*Bbw(sym_idx,user_idx) + sqrt(10^(-3))*(randn(Nt,1)+1i*randn(Nt,1))/sqrt(2);
+        Y(:,sym_idx,user_idx) = H(:,:,user_idx,user_idx)'*(upower(user_idx)*(Gu(:,user_idx))*Bu(sym_idx,user_idx) + mpower(user_idx)*(Gm(:,user_idx))*Bm(sym_idx,user_idx))+nnnn(:,sym_idx);
         for interf_idx = 1 : M
             if interf_idx ~= user_idx
-                Y(:,sym_idx,user_idx) = Y(:,sym_idx,user_idx) +  H(:,:,interf_idx, user_idx)'*Gu(:,interf_idx)*Bbw(sym_idx,interf_idx);
+                Y(:,sym_idx,user_idx) = Y(:,sym_idx,user_idx) +  H(:,:,interf_idx, user_idx)'*(upower(interf_idx)*(Gu(:,interf_idx))*Bu(sym_idx,interf_idx) + mpower(interf_idx)*(Gm(:,interf_idx))*Bm(sym_idx,interf_idx));
             end
         end
-    end    
-    Vu(:,user_idx) = inv(Y(:,:,user_idx)*Y(:,:,user_idx)')*Y(:,:,user_idx)*conj(Bbw(:,user_idx));
+    end
+    
+    Vu(:,user_idx) = (Y(:,:,user_idx)*Y(:,:,user_idx)')\Y(:,:,user_idx)*conj(Bu(:,user_idx));
+    Vu(:,user_idx) = (Vu(:,user_idx));
     Vu(:,user_idx) = Vu(:,user_idx)./norm(Vu(:,user_idx));
-    Vm(:,user_idx) = [0;0];
+    
+    Vm(:,user_idx) = inv(Y(:,:,user_idx)*Y(:,:,user_idx)')*Y(:,:,user_idx)*conj(Bm(:,user_idx));
+    Vm(:,user_idx) = (Vm(:,user_idx));
+    Vm(:,user_idx) = Vm(:,user_idx)./norm(Vm(:,user_idx));
+    
+    
+%     if (numiters == 4 && user_idx == 1)
+%                     mean(abs(Bu(:,1).' - Vu(:,1).'*Y(:,:,1)))
+%                 end
 end
--- LS_forward.m
+++ LS_forward.m
@@ -1,23 +1,28 @@
-function [Gu, Gm] = LS_forward(H, Vu, Vm, M1, n0, Bu, Bm, upower, mpower)
+function [Gu, Gm] = LS_forward(H, Vu, Vm, M1, n0, Bu, Bm, upower, mpower,mmmm)
 %update receive filters by least square algorithm
 
 [Nr,Nt,M,ignore] = size(H);
 
+Y = zeros(Nr,M1,M);
 
+Gu = zeros(Nr,M);
+Gm = zeros(Nr,M);
 for user_idx = 1 : M
     for sym_idx = 1:M1
-        Y(:,sym_idx,user_idx) = H(:,:,user_idx,user_idx)*(Vu(:,user_idx)*Bbw(sym_idx,user_idx)  ) + sqrt(n0)*(randn(Nr,1)+1i*randn(Nr,1))/sqrt(2);
+        Y(:,sym_idx,user_idx) = H(:,:,user_idx,user_idx)*(upower(user_idx)*Vu(:,user_idx)*Bu(sym_idx,user_idx) + mpower(user_idx)*Vm(:,user_idx)*Bm(sym_idx,user_idx)) + mmmm(:,sym_idx);
+        
         for interf_idx = 1 : M
             if interf_idx ~= user_idx
-                Y(:,sym_idx,user_idx) = Y(:,sym_idx,user_idx) +  H(:,:,user_idx,interf_idx)*Vu(:,interf_idx)*Bbw(sym_idx,interf_idx) ;
+                Y(:,sym_idx,user_idx) = Y(:,sym_idx,user_idx) +  H(:,:,user_idx,interf_idx)*(upower(interf_idx)*Vu(:,interf_idx)*Bu(sym_idx,interf_idx) + mpower(interf_idx)*Vm(:,interf_idx)*Bm(sym_idx,interf_idx));
             end
         end
-        
     end
     
-    Gu(:,user_idx) = inv(Y(:,:,user_idx)*Y(:,:,user_idx)')*Y(:,:,user_idx)*conj(Bbw(:,user_idx));
+    Gu(:,user_idx) = (Y(:,:,user_idx)*Y(:,:,user_idx)')\Y(:,:,user_idx)*conj(Bu(:,user_idx));
     Gu(:,user_idx) = Gu(:,user_idx)./norm(Gu(:,user_idx));
-    Gm(:,user_idx) = [0;0];
+    
+    Gm(:,user_idx) = (Y(:,:,user_idx)*Y(:,:,user_idx)')\Y(:,:,user_idx)*conj(Bm(:,user_idx));
+    Gm(:,user_idx) = Gm(:,user_idx)./norm(Gm(:,user_idx));
 end
 
     
